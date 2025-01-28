cc2d202583a47e355a0da11a0c7fce569c481645
Omar Hemedi <omarhemedijumaa@gmail.com>
2025-01-28 21:03:53 +0300

    Set up Next.js project with default structure

---
+++ Frontend/freelancer-connect/src/app/page.tsx
@@ -0,0 +1,11 @@
+import GuestLayout from "./layouts/guest/layout";
+
+export default function Home() {
+    return (
+        <GuestLayout>
+            <div>
+                <h1>Welcome to Freelancer-Connect</h1>
+            </div>
+        </GuestLayout>
+    );
+}
