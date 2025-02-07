872c918fd0f4d98f940bcf039542d4d0a7e7aebc
Omar Hemedi <omarhemedijumaa@gmail.com>
2025-02-07 22:05:58 +0300

    feat: integrate GuestLayout component in Home page
    - Import GuestLayout component
    - Wrap Home page content with GuestLayout for consistent layout structure

--- Frontend/freelancer-connect/src/app/page.tsx
+++ Frontend/freelancer-connect/src/app/page.tsx
@@ -1,36 +1,11 @@
 "use client";
 
 import GuestLayout from "./layouts/guest/layout";
-import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
-import HeroSection from "./layouts/guest/sections/hero";
-import AboutSection from "./layouts/guest/sections/about";
-import HowItWorksSection from "./layouts/guest/sections/howitworks";
-import WhyChooseUsSection from "./layouts/guest/sections/whychooseus";
-import MetricsSection from "./layouts/guest/sections/metrics";
-import TestimonialsSection from "./layouts/guest/sections/testimonials";
 
 export default function Home() {
     return (
         <GuestLayout>
-            {/* Hero Section */}
-            <HeroSection />
-
-            {/* About the System Section */}
-            <AboutSection />
-
-            {/* Why Choose Us Section */}
-            <WhyChooseUsSection />
-
-            {/* How It Works Section */}
-            <HowItWorksSection />
-
-            {/* Metrics Section */}
-            <MetricsSection />
-
-            {/* Testimonials Section */}
-            <TestimonialsSection />
+            <p>Hero section</p>
         </GuestLayout>
     );
 }
